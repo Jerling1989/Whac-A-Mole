579f48ca2c530972288d8262285f99f62fa3741c
Jacob <jerling1989@gmail.com>
2018-01-24 16:51:55 -0500

    JS comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,9 +20,7 @@ var carnival = new Audio('assets/audio/circustheme.mp3');
 var cheering = new Audio('assets/audio/cheering.mp3');
 
 
-
-
-// Timer Function For The Whole Game (60sec)
+// TIMER FUNCTION FOR THE GAME (45 SEC)
 function countdown() {
   intervalId = setInterval(decrement, 1000);
   function decrement() {
@@ -30,11 +28,13 @@ function countdown() {
   document.getElementById('timer').innerHTML = counter;
     if (counter === 0) {
       clearInterval(intervalId);
-      endScreen(); //make function to restart game
+      endScreen(); // LAUNCHES FINAL DISPLAY AT END OF GAME
     }
   }
 }
 
+// USER INSTRUCTION DISPLAY AT START OF GAME
+// RESET USER POINTS AND GAME TIMER
 function startScreen() {
   roundedScore = 0;
   document.getElementById('end-menu').style.display = 'none';
@@ -46,8 +46,10 @@ function startScreen() {
   
 }
 
+// LAUNCH INSTRUCTION DISPLAY AT PAGE LOAD
 startScreen();
 
+// LAUNCHES GAME AND HIDES INSTRUCTION DISPLAY ON BUTTON CLICK
 document.getElementById('start-button').onclick = function() {
   document.getElementById('start-menu').style.display = 'none';
   carnival.pause();
@@ -55,50 +57,50 @@ document.getElementById('start-button').onclick = function() {
   countdown();
 }
 
+// FINAL DISPLAY AT END OF GAME
 function endScreen() {
   document.getElementById('mole').style.display = 'none';
   document.getElementById('end-menu').style.display = 'block';
   cheering.play();
 }
 
+// LAUNCHES INSTRUCTION DISPLAY AT START OF GAME TO PLAY AGAIN
 document.getElementById('play-again').onclick = function() {
   startScreen();
 }
 
-
+// FUNCTION THAT CONTROLS MAJOR OPERATIONS OF THE GAME
 function shortGame() {
 
-  // setTimeout
+  // CREATES RANDOM INTERVAL FOR MOLE TO DISPLAY
   var x = Math.random();
   x = x * 1500;
   x = Math.floor(x);
 
-  console.log(x);
-  
-  // choose random mole
+  // CHOOSES RANDOM MOLE IMAGE FROM ARRAY
   var y = Math.random();
   y = y * 6;
   y = Math.floor(y);
 
-  console.log(moles[y]);
-
+  // ADD CORRESPONDING AUDIO TO MOLE IMAGE
   var moleAudio = new Audio(sounds[y]);
 
-
+  // CREATES RANDOM NUMBER ON Y AXIS FOR MOLE TO DISPLAY
   var top = Math.random();
   top = top * 390;
   top = Math.floor(top);
 
+  // CREATES RANDOM NUMBER ON X AXIS FOR MOLE TO DISPLAY
   var left = Math.random();
   left = left * 640;
   left = Math.floor(left);
 
-
+  // IF STATEMENT TO STOP NEW MOLE CREATION NEAR END OF GAME
   if (counter < 2) {
     document.getElementById('mole').style.display = 'none';
 
   } else {
-
+    // FUNCTION TO CREATE MOLE IMAGE AND LOCATION
     setTimeout(function() {
       document.getElementById('mole').style.top = top + 'px';
       document.getElementById('mole').style.left = left + 'px';
@@ -107,24 +109,27 @@ function shortGame() {
       moleAudio.play();
       createdTime = Date.now();
     }, x);
-
-  }
-  
+  } 
 
 }
 
-
+// KEEPS SCORE WHEN USER CLICKS ON MOLE
+// TRIGGERS FUNCTION TO CREATE A NEW MOLE
 document.getElementById('mole').onclick = function() {
 
+  // CALCULATES SCORE
   clickedTime = Date.now();
   reactionTime = (clickedTime - createdTime) / 10;
   userPoints = 150 - reactionTime;
   totalPoints = totalPoints + userPoints;
   roundedScore = Math.floor(totalPoints);
 
+  // DISPLAYS SCORE TO USER
   document.getElementById('currentScore').innerHTML = roundedScore;
   document.getElementById('final-score').innerHTML = roundedScore;
 
+  // MAKE MOLE DISAPPEAR
   this.style.display = 'none';
+  // LAUNCHES FUNCTION TO CREATE NEW MOLE
   shortGame();
 }
